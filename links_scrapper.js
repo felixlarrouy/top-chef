969f51a00ee14e2297175723767a697eea9aec4b
felixlarrouy <felix.larrouy@gmail.com>
2018-02-14 11:31:32 +0100

    feature(module) : able to scrap a restaurant promotion on lafourchette, but not all restaurants

--- links_scrapper.js
+++ links_scrapper.js
@@ -2,8 +2,6 @@ const cheerio = require('cheerio')
 const request = require('request');
 const fs = require('fs');
 
-var num_pages = 0;
-
 request({
   uri: "https://restaurant.michelin.fr/restaurants/france/restaurants-1-etoile-michelin/restaurants-2-etoiles-michelin/restaurants-3-etoiles-michelin",
 }, function(error, response, body) {
@@ -31,7 +29,6 @@ request({
       var $ = cheerio.load(body);
       $('.poi-card-link').each(function(index) {
         var link = $(this);
-        //restaurant['title'] = $(link).find('.poi_card-display-title').text().trim();
         var restaurant_link = "https://restaurant.michelin.fr" + link.attr('href');
         try {
           fs.appendFile("restaurant_links.txt", restaurant_link + "\n");
@@ -43,4 +40,104 @@ request({
       console.log(err)
     }).end()
   }
-});
+}).on('error', function(err) {
+  console.log(err)
+}).end();
+
+/*const frenchStarredRestaurantsUrl = "https://restaurant.michelin.fr/restaurants/france/restaurants-1-etoile-michelin/restaurants-2-etoiles-michelin/restaurants-3-etoiles-michelin"
+var numberOfPages = -1
+
+exports.getMichelinRestaurants = (callback) => {
+  getNumberOfPages(frenchStarredRestaurantsUrl).then(nPages => {
+    const promises = []
+    for (var i = 1; i <= nPages; i++) {
+      promises.push(getRestaurantsUrls(i))
+    }
+    return Promise.all(promises)
+  }).then(restaurantLinks => {
+    const promises = []
+    for (var i = 0; i < restaurantLinks.length; i++) {
+      promises.push(getRestaurantsInfos(restaurantLinks[i]))
+    }
+    return Promise.all(promises)
+  }).then(restaurants => {
+    return saveToJSON(restaurants)
+  }).then(string => {
+    callback(null, string)
+  }).catch(err => {
+    callback(err)
+  })
+}
+
+function getNumberOfPages(url) {
+  return new Promise(function(resolve, reject) {
+    request({
+      uri: "https://restaurant.michelin.fr/restaurants/france/restaurants-1-etoile-michelin/restaurants-2-etoiles-michelin/restaurants-3-etoiles-michelin",
+    }, function(err, resp, body) {
+      if (err) reject(error)
+      const $ = cheerio.load(body);
+
+      $(".mr-pager-link").each(function() {
+        if (numberOfPages < parseInt($(this).attr("attr-page-number"))) {
+          numberOfPages = parseInt($(this).attr("attr-page-number"))
+        }
+      })
+      if (numberOfPages == -1) {
+        resolve(numberOfPages)
+      } else {
+        reject(new Error("Can't find the number of pages."))
+      }
+    })
+  })
+}
+
+function getRestaurantsUrls(pageNumber) {
+  return new Promise(function(resolve, reject) {
+    request({
+      uri: frenchStarredRestaurantsUrl + "/page-" + pageNumber,
+    }, function(err, resp, body) {
+      if (err) reject(error)
+      const $ = cheerio.load(body)
+      var links = []
+      $('.poi-card-link').each(function(index) {
+        links.push("https://restaurant.michelin.fr" + $(this).attr('href'));
+      })
+      resolve(links)
+    })
+  })
+}
+
+function getRestaurantsInfos(url) {
+  return new Promise(function(resolve, reject) {
+    request({
+      uri: url,
+    }, function(err, resp, body) {
+      if (err) reject(error)
+      const $ = cheerio.load(body)
+      var restaurant = {}
+      restaurant.name = $('.poi_intro-display-title').text().trim();
+      restaurant.address = {};
+      restaurant.address.thoroughfare = $('.poi_intro-display-address .field__items .thoroughfare').text();
+      restaurant.address.postalcode = $('.poi_intro-display-address .field__items .postal-code').text();
+      restaurant.address.locality = $('.poi_intro-display-address .field__items .locality').text();
+
+      if (restaurant != {}) {
+        resolve(restaurant)
+      } else {
+        reject("Cannot find restaurant")
+      }
+    })
+  })
+}
+
+function saveToJSON(restaurantList) {
+  return new Promise(function(resolve, reject) {
+    for (var i = 0; i < restaurantList.length; i++) {
+      try {
+        fs.appendFile("restaurant.json", restaurantList[i] + "\n");
+      } catch (err) {
+        console.log(err);
+      }
+    }
+  })
+}*/
--- modules/lafourchette.js
+++ modules/lafourchette.js
@@ -2,15 +2,66 @@ const cheerio = require('cheerio')
 const request = require('request');
 const fs = require('fs');
 
-var separators = ['\'', ' - ', ' '];
-var regExp = new RegExp('[' + separators.join('') + ']', 'g')
+let separators = ['\'', ' ', '-'];
+let regExp = new RegExp('[' + separators.join('') + ']', 'g')
 
 var lineReader = require('readline').createInterface({
   input: require('fs').createReadStream('./restaurant_info.json')
 });
 
 lineReader.on('line', function(line) {
-  var restaurant = JSON.parse(line);
-  var tokens = restaurant["name"].toLowerCase().normalize('NFD').replace(/[\u0300-\u036f]/g, "").split(regExp);
-  console.log(tokens);
+  var restaurant_to_search = JSON.parse(line);
+  var name = encodeURIComponent(restaurant_to_search["name"])
+  // var tokensAPI = restaurant_to_search["name"].toLowerCase().normalize('NFD').replace(/[\u0300-\u036f]/g, "").replace(/ - /g, '-').split(regExp);
+  // var linkParamatersAPI = "";
+  // for (var i = 0; i < tokensAPI.length - 1; i++) {
+  //   linkParamatersAPI += tokensAPI[i] + '+';
+  // }
+  // linkParamatersAPI += tokensAPI[tokensAPI.length - 1];
+  request({
+    //uri: "https://www.lafourchette.com/recherche/autocomplete?searchText=" + linkParamatersAPI + "&localeCode=fr",
+    uri: "https://m.lafourchette.com/api/restaurant-prediction?name=" + name,
+  }, function(error, response, body) {
+    if (error) return console.log(error);
+    var $ = cheerio.load(body);
+    var result = JSON.parse(body);
+    var restaurants_result = result['data']['restaurants'];
+    for (var i = 0; i < restaurants_result.length; i++) {
+      // trouver le restaurant de la liste (s'il existe) avec le même zipcode et la même ville
+      if (restaurants_result[i]['zipcode'] == restaurant_to_search['address']['postalcode']) {
+        let matching_resto = restaurants_result[i]
+        let tokensSearch = matching_resto["name"].toLowerCase().normalize('NFD').replace(/[\u0300-\u036f]/g, "").replace(/ - /g, '-').split(regExp);
+        var searchLinkParameters = "";
+        for (var i = 0; i < tokensSearch.length - 1; i++) {
+          searchLinkParameters += tokensSearch[i] + '-';
+        }
+        searchLinkParameters += tokensSearch[tokensSearch.length - 1];
+        // aller sur la page de ce restaurant
+        request({
+          uri: "https://www.lafourchette.com/restaurant/" + searchLinkParameters + "/" + matching_resto['id_restaurant'],
+        }, function(error, response, body) {
+          if (error) return console.log(error);
+          var $$ = cheerio.load(body);
+          // scrapper promotions and events
+          var restaurant = {}
+          restaurant['name'] = matching_resto['name']
+          var address = {}
+          address['city'] = matching_resto['city']
+          address['zipcode'] = matching_resto['zipcode']
+          restaurant['address'] = address
+          restaurant['event'] = $$('.saleType.saleType--event .saleType-title').text()
+          restaurant['promotions'] = $$('.saleType.saleType--specialOffer .saleType-title').text()
+          try {
+            fs.appendFile("restaurant_lafourchette.json", JSON.stringify(restaurant) + "\n");
+          } catch (err) {
+            console.log(err);
+          }
+        }).on('error', function(err) {
+          console.log(err)
+        }).end()
+      }
+    }
+  }).on('error', function(err) {
+    console.log(err)
+  }).end()
 });
